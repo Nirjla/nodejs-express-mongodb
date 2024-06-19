c1c355f6c32dc94f22241899760687c98447bfa2
Nirjla <shakyanirjala6@gmail.com>
2024-06-19 19:54:18 +0545

    initial server setup

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors"
+
+const app = express();
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+      res.json({ message: "Welcome to nirjla's api" })
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+})
